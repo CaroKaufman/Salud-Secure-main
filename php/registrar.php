e194f6af6aec40d8394b294d63d720c4df62cd5d
CaroKaufman <101335676+CaroKaufman@users.noreply.github.com>
2022-09-01 10:42:03 -0300

    g

---
+++ php/registrar.php
@@ -0,0 +1,38 @@
+<?php
+
+$con = new mysqli("localhost", "root", "rootroot");
+mysqli_select_db($con,"saludsecure");
+
+if ($con->connect_error){
+	die("Connection failed: ".$con ->connect_error);
+}
+
+$data=array();
+
+    												
+	$sql="SELECT IDpaciente from paciente where usuario='".$_REQUEST['usu']."' AND contrasena='".$_REQUEST['pass']."'";
+	$res=$con->query($sql);
+	
+	if($res->num_rows > 0){ 		//ya existe un usuario														
+											
+		$data['status']='err';
+    	$data['result']= '';
+	}else{							//como no existe el usuario, va a registrarse con ese nombre, por lo tanto ejecutamos y preparamos al sql.
+		$sql= "INSERT INTO paciente (usuario,contrasena,nombre,apellido,credencial,nacimiento) VALUES ('".$_REQUEST['usu']."','".$_REQUEST['pass']."','".$_REQUEST['nom']."','".$_REQUEST['ape']."','".$_REQUEST['Credencial']."','".$_REQUEST['FechadeNacimiento']."')"; //'".$_REQUEST['DNI']."',
+			$res=$con->query($sql);
+			if($res==1){
+				$data['status']='ok';
+    			$data['result']= '1';
+			}else{
+				$data['status']='error';
+    			$data['result']= '1';
+			}
+	}
+	echo json_encode($data);
+	$con->close();	
+	
+?>
+
+
+
+
---
+++ php/validar.php
@@ -0,0 +1,31 @@
+<?php
+
+$con = new mysqli("localhost", "root", "rootroot");
+mysqli_select_db($con,"saludsecure");
+
+if ($con->connect_error){
+	die("Connection failed: ".$con ->connect_error);
+}
+
+$data=array();
+
+//if ($_REQUEST ['que']=='L'){     															//el REQUEST es lo que le llega de la pantalla
+	$sql="SELECT IDpaciente, nombre, apellido from paciente where usuario='".$_REQUEST['usu']."' and contrasena='".$_REQUEST['pass']."'";	
+																							//$res =$con -> query($sql);   //con el query le llega a la bbdd la sentencia de arriba. Lo devuelve en forma organizada.
+	//echo $sql;
+	$res=$con->query($sql);
+	$i=0;
+	if($res->num_rows > 0){ 																//num_rows me da el numero de filas como 		resultado, me da la cantidad de filas de res, nos sirve para saber si existen filas o no.
+		$userData = $res->fetch_assoc(); 												    
+		$data['status']='ok';
+    	$data['result']= $userData;
+	}else{
+		$data['status']='err';
+    	$data['result']= '';
+	}
+	//retorno los datos en formato JSON
+	echo json_encode($data);
+	$con->close();						                                                       
+
+?>
+
